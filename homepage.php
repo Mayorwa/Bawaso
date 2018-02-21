2a434c473c96a5f3cf0320ddfc76adc3a947938d
Ogunwole Olumayowa <mayorviper.mo@gmail.com>
2018-02-22 00:34:40 +0100

    Allof Them

---
+++ homepage.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BAWASO</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="css/font-awesome.min.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    </head>
+    <body>
+        <header>
+        <div class="fhsco-navbar">
+        <div class="container">
+            <nav class="navbar navbar-default" role="navigation"> 
+                <div class="navbar-header"> 
+                   <button type="button" class="navbar-toggle" data-toggle="collapse"  
+                      data-target="#example-navbar-collapse">
+                      <span class="sr-only">Toggle navigation</span> 
+                      <span class="icon-bar"></span> 
+                      <span class="icon-bar"></span> 
+                      <span class="icon-bar"></span>
+                    </button> 
+                    <a class="navbar-brand" href="#"><img src="imgs/logo.png" class="logo"></a> 
+                 </div> 
+                 <div class="collapse navbar-collapse navbar-right" align="center" id="example-navbar-collapse"> 
+                    <ul class="nav navbar-nav"> 
+                       <li><a href="#">ABOUT US</a></li> 
+                       <li><a href="#">HOW IT WORKS</a></li>
+                       <li><a href="#">CONTACT</a></li>
+                       <li><a href="#">LOG IN</a></li>
+                       <li><button class="btn btn-primary">SIGN UP</button></li>
+                    </ul> 
+                 </div> 
+              </nav> 
+              </div>
+</div>
+              <div class="fhsco-bigtrumpodrum img-responsive">
+                    <div class="fhsco-background">
+                            <h1>Find and Book Talents</h1>
+                            <p>Dedicated promoting and showcasing talent and human resources from around the world</p>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="what talent are you looking for?"> 
+                                <button class="btn btn-primary">SEARCH</button>
+                            </div>
+                    </div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+              </div>
+        </header>
+        <section class="fhsco-mainbody">
+            <h2>Explore the Categories</h2>
+            
+            <div class="container-fluid fhsco-talent-fulldiv">
+                <div class="">
+                <div class="col-md-2"></div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Musicians.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">MUSICIANS</p>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Comedians.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">COMEDIANS</p>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Actors.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">ACTORS</p>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/On Air Personalities.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">ON AIR PERSONALITIES</p>
+                    </div>
+                    <div class="col-md-2"></div>
+                </div>
+            </div>
+            <div class="container-fluid fhsco-talent-fulldiv">
+                <div class="">
+                <div class="col-md-2"></div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Graphic Designers.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">GRAPHIC DESIGNERS</p>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Spiritual Leaders.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">SPIRITUAL LEADERS</p>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Coach-Motivational Leaders.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">MOTIVATIONAL SPEAKERS</p>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-xs-6">
+                        <img src="imgs/Business Owners.png" class="fhsco-talent-pictures">
+                        <p class="fhsco-whichpeople">BUSINESS OWNERS</p>
+                    </div>
+                    <div class="col-md-2"></div>
+                </div>
+            </div>
+            <div class="fhsco-buttondiv">
+                <button>SEE ALL CATEGORIES</button>
+            </div>
+        </section>
+        <section class="fhsco-secondmain">
+            <h3>How It Works</h3>
+            <div align="center">
+                <div class="fhsco-howitworks">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-2">
+                                <div class="fhsco-fullprocedures">
+                                    <img src="imgs/find.png" class="fhsco-works-pictures1">
+                                    <p class="fhsco-steps">FIND</p>
+                                    <p class="fhsco-works-procedures">Explore our categories to find the right talent to match your requierments</p>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="fhsco-fullprocedures">
+                                    <img src="imgs/book.png" class="fhsco-works-pictures2">
+                                    <p class="fhsco-steps">BOOK</p>
+                                    <p class="fhsco-works-procedures">Browse profile then book your favourite talent</p>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="fhsco-fullprocedures">
+                                    <img src="imgs/pay.png" class="fhsco-works-pictures3">
+                                    <p class="fhsco-steps">PAY</p>
+                                    <p class="fhsco-works-procedures">Payment happen through our secure payment gateway</p>
+                                </div>
+                            </div>
+                            <div class="col-md-3"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fhsco-thirdmain">
+                <div class="fhsco-quotes-img">
+                        <img src="imgs/Testimonial Quote.png">
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <p class="fhsco-testimonial">Bawaso.com is an efficient and reliable platform to land your favourite celebrities. I contacted them for A List Artist and I got a feedback in 3hours. Bravo!</p>
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="container fhsco-fullprofile" align="center">
+                        <div class="row">
+                            
+                            <div class="col-md-4 col-sm-6 col-xs-12">
+                                <div class="fhsco-peopletest-div1" align="center">
+                                    <img src="imgs/Imgprofile.jpg" class="fhsco-testimonial-pictures">
+                                    <div class="fhsco-peoplesname">
+                                    <p>Amanda Wiliams</p>
+                                    <p>Founder, Giant Media</p>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6 col-xs-12">
+                                <div class="fhsco-peopletest-div2" align="center">
+                                    <img src="imgs/Imgprofile.jpg" class="fhsco-testimonial-pictures">
+                                    <div class="fhsco-peoplesname">
+                                    <p>Deji Adeogun</p>
+                                    <p>Co-Founder, Printivo.com</p>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6 col-xs-12">
+                                <div class="fhsco-peopletest-div3" align="center">
+                                    <img src="imgs/Imgprofile.jpg" class="fhsco-testimonial-pictures">
+                                    <div class="fhsco-peoplesname">
+                                        <p>Olusola Amusan</p>
+                                        <p>Founder, Coven Labs</p>
+                                    </div>
+                                </div>
+                            </div>
+</div>
+                    </div>
+        </section>
+        <main class="footer">
+                <div class="container">
+                <div class="">
+                    <div class="col-md-6 col-sm-12 fhsco-footer-text">
+                      <h4>Hire Or Get Hired!</h4>
+                      <p>Are you a brand looking to hire a talent or a talent looking to get hired?</p>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="fhsco-footer-button">
+                            <button>REGISTER NOW</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+        </main>
+        <footer>
+            <div class="container">
+                <div class="col-md-3"></div>
+                <div class="col-md-7">
+                <div class="footer-social">
+                    <ul>
+                        <li><img src="imgs/twitter.png" class="social-mgs"></li>
+                        <li><img src="imgs/facebook.png" class="social-mgs"></li>
+                        <li><img src="imgs/instagram.png" class="social-mgs"></li>
+                        <li><img src="imgs/linkedin.png" class="social-mgs"></li>
+                    </ul>
+                </div>
+                <div class="footer-list">
+                    <ul>
+                        <li>ABOUT US</li>
+                        <li class="list-with-border">FAQ</li>
+                        <li class="list-with-border">CATEGORIES</li>
+                        <li class="list-with-border">HOW IT WORKS</li>
+                        <li class="list-with-border">PRIVACY POLICY</li>
+                        <li class="list-with-border">CONTACT US</li>
+                    </ul>
+                </div>
+                <p class="copyright">© 2018 Bawaso Limited. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+            </footer>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </body>
+</html>
